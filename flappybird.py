9e060821eddd7c7ec1aa0569b70e056aa47b8c32
Lukáš Hajnyš <hajnysl22@spstrutnov.cz>
2023-02-01 16:29:56 +0100

    Created game window + game main loop and also blitted it

--- flappybird.py
+++ flappybird.py
@@ -1,2 +1,13 @@
 import pygame
 pygame.init()
+
+width = 600
+height = 600 
+screen = pygame.display.set_mode((width, height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.fill((255, 255, 255))        
