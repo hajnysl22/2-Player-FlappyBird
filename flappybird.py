6f93f798f30b578887d1f67e4e7016430846dfab
Lukáš Hajnyš <hajnysl22@spstrutnov.cz>
2023-02-01 16:36:46 +0100

    Renamed pygame window + changed window's icon

--- flappybird.py
+++ flappybird.py
@@ -1,13 +1,25 @@
 import pygame
 pygame.init()
 
+# Game Window's variables
 width = 600
 height = 600 
+
+# Game Window
 screen = pygame.display.set_mode((width, height))
 
+pygame.display.set_caption('Flappy Bird')
+flappybird_icon = pygame.image.load("flappybird_icon.png")
+
+pygame.display.set_icon(flappybird_icon)
+
+# Main Game Loop
 while True:
+    # Easily exit the game 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    # Game Window     
     screen.fill((255, 255, 255))        
